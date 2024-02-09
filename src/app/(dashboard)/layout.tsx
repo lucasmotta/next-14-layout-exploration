0e9d876b38394361f524e50f8e751304af29e2a9
Lucas Motta <mail@lucasmotta.com>
2024-02-09 13:48:20 -0300

    Add some Sidebar links

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,17 +1,39 @@
-import { Box, Grid, Heading, Text } from '@radix-ui/themes'
+import { Box, Button, Grid, Heading, Text } from '@radix-ui/themes'
+import { GearIcon } from '@radix-ui/react-icons'
+import Link from 'next/link'
 import { ReactNode } from 'react'
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <Grid columns="240px 1fr">
-      <Box
+      <Grid
         p="4"
+        justify="start"
+        rows="fit-content(100%)"
         style={{ minHeight: '100dvh', backgroundColor: 'var(--gray-a2)' }}
       >
-        <Text>Sidebar nav</Text>
-      </Box>
+        <Heading size="1" mb="2">
+          Top level
+        </Heading>
+        <NavLink href="/settings/team">
+          <GearIcon /> Settings
+        </NavLink>
+      </Grid>
 
       <Box p="4">{children}</Box>
     </Grid>
   )
 }
+
+const NavLink = (props: { href: string; children: ReactNode }) => (
+  <Button
+    variant="ghost"
+    color="gray"
+    highContrast
+    {...props}
+    asChild
+    style={{ justifyContent: 'flex-start', gap: 'var(--space-2)' }}
+  >
+    <Link href={props.href}>{props.children}</Link>
+  </Button>
+)
