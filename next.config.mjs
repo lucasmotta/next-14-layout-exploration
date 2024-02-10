05f4850d5572b1563231c0c688f9929c10b01087
Lucas Motta <mail@lucasmotta.com>
2024-02-09 21:44:48 -0300

    Use nextjs config for url rewriting

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/settings',
+        destination: '/settings/team',
+      },
+      {
+        source: '/:envId/organizations/:orgId',
+        destination: '/:envId/organizations/:orgId/features',
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
