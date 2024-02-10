7385b506f0353a7152ff8d49e7bed0e8f5eb4989
Lucas Motta <mail@lucasmotta.com>
2024-02-09 21:47:01 -0300

    Remove some dependencies

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,14 +1,12 @@
-import { Box, Heading } from '@radix-ui/themes'
+import { Box, Card, Flex, Heading } from '@radix-ui/themes'
 import { ReactNode } from 'react'
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <Box p="4">
-      <Heading size="6" mb="4">
-        Auth layout
-      </Heading>
-
-      {children}
-    </Box>
+    <Flex p="4" align="center" justify="center" style={{ minHeight: '100dvh' }}>
+      <Card style={{ width: 420 }} variant="classic">
+        {children}
+      </Card>
+    </Flex>
   )
 }
